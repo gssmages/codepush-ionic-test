9e8c113ad82bc06fc836fbe9be90f261aaefa71a
gssmages <32328556+gssmages@users.noreply.github.com>
2019-04-03 17:34:45 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { CodePush, InstallMode, SyncStatus } from '@ionic-native/code-push/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,7 +12,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private codePush: CodePush
   ) {
     this.initializeApp();
   }
@@ -21,6 +22,26 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.checkCodePush(); //Use the plugin always after platform.ready()
     });
   }
+  checkCodePush() {
+    
+    this.codePush.sync({
+     updateDialog: {
+      appendReleaseDescription: true,
+      descriptionPrefix: "\n\nChange log:\n"   
+     },
+     installMode: InstallMode.IMMEDIATE
+  }).subscribe(
+    (data) => {
+     console.log('CODE PUSH SUCCESSFUL: ' + data);
+     
+    },
+    (err) => {
+     console.log('CODE PUSH ERROR: ' + err);
+     
+    }
+  );
+ }
 }
